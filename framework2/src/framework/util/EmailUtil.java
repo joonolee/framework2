88153e3301a976434739005dd8afbd50c910df84
Jun-Ho Lee <joono_lee@naver.com>
2013-01-31 15:35:23 +0900

    이메일 전송시 한글파일 깨즘 증상해결

--- framework2/src/framework/util/EmailUtil.java
+++ framework2/src/framework/util/EmailUtil.java
@@ -20,6 +20,7 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.MimeUtility;
 
 /**
  * JavaMail을 이용해 메일을 발송하는 유틸리티 클래스이다.
@@ -31,7 +32,7 @@ public class EmailUtil {
 	 */
 	private EmailUtil() {
 	}
-	
+
 	/**
 	 * 기본 인코딩 값
 	 */
@@ -356,7 +357,7 @@ public class EmailUtil {
 				BodyPart fileBodyPart = new MimeBodyPart();
 				FileDataSource fds = new FileDataSource(f);
 				fileBodyPart.setDataHandler(new DataHandler(fds));
-				fileBodyPart.setFileName(f.getName());
+				fileBodyPart.setFileName(MimeUtility.encodeText(f.getName(), charset, "B"));
 				multipart.addBodyPart(fileBodyPart);
 			}
 			message.setContent(multipart);
