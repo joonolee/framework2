a52e4d5c9399e3ed86f2922a5e5d0e44d7ebe86e
JunHo Lee <joono_lee@naver.com>
2015-07-20 10:31:10 +0900

    DateUtil 추가

---
+++ framework2/src/framework/util/DateUtil.java
@@ -0,0 +1,118 @@
+package framework.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * 날짜관련 유틸리티 클래스이다.
+ * @Author	: JCJ
+ */
+public class DateUtil {
+
+	/**
+	 * 날짜여부 체크
+	 * @param year : 년
+	 * @param month : 월
+	 * @param day : 일
+	 * @return boolean : 날짜여부
+	 */
+	public static boolean isDate(int year, int month, int day) {
+		return (toDate(year, month, day) != null);
+	}
+
+	/**
+	 * 날짜여부 체크
+	 * @param dateStr : 년월일
+	 * @return boolean : 날짜여부
+	 */
+	public static boolean isDate(String dateStr) {
+		return (toDate(dateStr) != null);
+	}
+
+	/**
+	 * 날짜여부 체크
+	 * @param dateStr : 년월일
+	 * @param format : 날짜형식(ex : yyyyMMdd, yyyy-MM-dd...)
+	 * @return boolean : 날짜여부
+	 */
+	public static boolean isDate(String dateStr, String format) {
+		return (toDate(dateStr, format) != null);
+	}
+
+	/**
+	 * 원하는 날짜 Date 생성
+	 * @param year : 년
+	 * @param month : 월
+	 * @param day : 일
+	 * @return Date : 원하는 날짜의 Date객체
+	 */
+	public static Date toDate(int year, int month, int day) {
+		Calendar cal = Calendar.getInstance();
+		cal.clear();
+		cal.setLenient(false);
+		cal.set(year, month - 1, day);
+		return cal.getTime();
+	}
+
+	/**
+	 * 원하는 날짜 Date 생성
+	 * @param dateStr : 년월일(yyyy-MM-dd)
+	 * @return Date : 원하는 날짜의 Date객체
+	 */
+	public static Date toDate(String dateStr) {
+		return toDate(dateStr.replaceAll("[-|/|.]", ""), "yyyyMMdd");
+	}
+
+	/**
+	 * 원하는 날짜 Date 생성
+	 * @param dateStr : 년월일
+	 * @param format : 날짜형식(ex : yyyyMMdd, yyyy-MM-dd...)
+	 * @return Date : 원하는 날짜의 Date객체
+	 */
+	public static Date toDate(String dateStr, String format) {
+		SimpleDateFormat sdf = new SimpleDateFormat(format);
+		sdf.setLenient(false);
+		Date date = new Date();
+		try {
+			date = sdf.parse(dateStr);
+		} catch (ParseException pe) {
+			date = null;
+		}
+		return date;
+	}
+
+	/**
+	 * Date 객체를 String으로 변환(yyyy-MM-dd)
+	 * @param date : 날짜객체
+	 * @return String : 날짜의 String 객체(yyyy-MM-dd)
+	 */
+	public static String toString(Date date) {
+		return toString(date, "yyyy-MM-dd");
+	}
+
+	/**
+	 * Date 객체를 String으로 변환
+	 * @param date : 날짜객체
+	 * @param format : 원하는 날짜형식(ex : yyyyMMdd, yyyy-MM-dd...)
+	 * @return String : 날짜의 String 객체
+	 */
+	public static String toString(Date date, String format) {
+		SimpleDateFormat sdf = new SimpleDateFormat(format);
+		String dateStr = "";
+		dateStr = sdf.format(date);
+		return dateStr;
+	}
+
+	/**
+	 * 두 날짜 사이의 차
+	 * @param startday : 시작일
+	 * @param endday : 종료일
+	 * @return 두 날짜 사이의 차
+	 */
+	public static long getDateDiff(Date startday, Date endday) {
+		long diff = endday.getTime() - startday.getTime();
+		return (diff / (1000 * 60 * 60 * 24));
+	}
+}
